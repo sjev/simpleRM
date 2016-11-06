f0128498895898b6d4410c411a7110ac82c8f57d
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2016-11-06 19:41:19 +0100

    starting on build_docs script

---
+++ src/build_docs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+script to build documentation using simpleRM
+
+@author: Jev Kuznetsov
+"""
+
+from simpleRM import DataProvider
+
+fName = '../requirements/simpleSE.yml'
+
+dp = DataProvider(fName)
+
+print('done')
--- src/simpleRM.py
+++ src/simpleRM.py
@@ -7,3 +7,45 @@ main simpleRM module
 
 """
 
+import yaml
+import loader
+
+def walk_gen(d, parent='ROOT'):
+    """ tree walker, used to traverse through requirement trees """
+    for k in sorted(d.keys()):
+        v = d[k]
+        yield Requirement(k,parent,v)
+        
+        if 'requirements' in v.keys():
+            for r in walk_gen(v['requirements'],parent=k) :
+                yield r
+
+
+class Requirement():
+    
+    def __init__(self,tag,parent,properties):
+        """ 
+        tag: identifier, parent: parent requirement tag, properties: dict of parameters 
+        """
+        self.tag = tag # unique identifier
+        self.parent = parent # parent requirement
+        
+        if 'requirements' in properties.keys():
+            self.children = list(properties['requirements'].keys())
+        else:
+            self.children = None
+    
+    def __repr__(self):
+        return '%s parent:%s children:%s' % (self.tag,self.parent, str(self.children))
+        
+        
+class DataProvider():
+    """ parses trees and returns data upon request """
+    
+    def __init__(self, fName):
+        
+        self._data = yaml.load(open(fName,'r'),loader.Loader) 
+    
+        self.requirements = [r for r in walk_gen(self._data['requirements'])]
+    
+        
